cf0148fcf8bb4beaa641109e57af297056f7b37d
juhovaananen <n3vaju00@students.oamk.fi>
2024-02-11 19:13:32 +0200

    Projekti valmis

--- js/functions.js
+++ js/functions.js
@@ -1,38 +1,41 @@
-' use strict'
+'user strict'
 
-let rand_num1 = 0;
-let rand_num2 = 0;
-let correctAnswers = 0;
-let incorrectAnswers = 0;
+let rand_num1 = 0
+let rand_num2 = 0
+let wrong = 0
+let correct = 0
 
-const getRandomIntNumberInRange = (min, max) => {
+const CountCorrect = document.querySelector('#correct')
+const CountWrong = document.querySelector('#wrong')
+
+const getRandomIntNumberInRange = (min,max) => {
     return Math.floor(Math.random() * max) + min;
-};
-
+}
 const randomizeNumbers = () => {
-    rand_num1 = getRandomIntNumberInRange(1, 10);
-    rand_num2 = getRandomIntNumberInRange(1, 10);
-    document.querySelector('#num1').innerHTML = rand_num1;
-    document.querySelector('#num2').innerHTML = rand_num2;
-};
+    rand_num1 = getRandomIntNumberInRange(1,10)
+    rand_num2 = getRandomIntNumberInRange(1,10)
+    document.querySelector('#num1').innerHTML = rand_num1
+    document.querySelector('#num2').innerHTML = rand_num2
+}
 
 addEventListener("DOMContentLoaded", () => {
-    randomizeNumbers();
+    randomizeNumbers()
 });
 
-document.querySelector('button').addEventListener('click', () => {
-    const answer = Number(document.querySelector('input').value);
-    const correctAnswer = rand_num1 + rand_num2;
+document.querySelector('button').addEventListener('click',() => {
+    const answer = Number(document.querySelector('input').value)
+    const correctAnswer = rand_num1 + rand_num2
     if (answer === correctAnswer) {
-        correctAnswers++;
-        document.querySelector('#correct').innerHTML = Correct answers: ${correctAnswers};
-        alert('Correct!');
-    } else {
-        incorrectAnswers++;
-        document.querySelector('#incorrect').innerHTML = Incorrect answers: ${incorrectAnswers};
-        alert('Incorrect!');
+        alert('Correct!')
+        correct++
+    }   else {
+        alert('Incorrect!')
+        wrong++
     }
 
-    randomizeNumbers();
-    document.querySelector('input').value = '';
-});
+    CountCorrect.innerHTML = correct;
+    CountWrong.innerHTML = wrong;
+
+    randomizeNumbers()
+    document.querySelector('input').value=''
+})
